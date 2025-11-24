1d1155ebe59e5913f52f03856cd39e6b6397378f
Rexshimura <mortpauljpm03@gmail.com>
2025-11-24 15:53:07 +0800

    New Routes

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from "./pages/Landing.jsx";
+import Home from "./pages/Main/Home.jsx"
+import SigninPage from "./pages/Auth/Sign-In.jsx";
+import SignupPage from "./pages/Auth/Sign-Up.jsx";
 import './App.css';
 
 function App() {
@@ -9,7 +12,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
 
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </div>
     </Router>
